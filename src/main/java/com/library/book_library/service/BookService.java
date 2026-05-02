6d96546e787432d48bcd7c230286997770b55c2f
Vijay Sharma <thewijaysharma@gmail.com>
2026-05-02 13:38:49 +0530

    remove hardcoded list

--- src/main/java/com/library/book_library/service/BookService.java
+++ src/main/java/com/library/book_library/service/BookService.java
@@ -2,27 +2,26 @@ package com.library.book_library.service;
 
 import com.library.book_library.exception.BookNotFoundException;
 import com.library.book_library.model.Book;
+import com.library.book_library.repository.BookRepository;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class BookService {
 
-    private final List<Book> books = new LinkedList<>(List.of(
-            new Book(1L, "Clean Code", "Robert C. Martin"),
-            new Book(2L, "The Pragmatic Programmer", "Andrew Hunt"),
-            new Book(3L, "Designing Data-Intensive Applications", "Martin Kleppmann")
-    ));
+
+    private final BookRepository repository;
+
+    public BookService(BookRepository repository) {
+        this.repository = repository;
+    }
 
     @Nonnull
     public Book getBookById(Long id) {
-        return books.stream()
-                .filter(book -> book.getId().equals(id))
-                .findFirst()
+        return repository.findById(id)
                 .orElseThrow(() -> new BookNotFoundException(id));
     }
 
@@ -30,27 +29,24 @@ public class BookService {
         String authorSearch = authorName != null ? authorName.toLowerCase() : null;
         String titleSearch = bookTitle != null ? bookTitle.toLowerCase() : null;
 
-        return books.stream()
+        return repository.findAll().stream()
                 .filter(book -> authorSearch == null || book.getAuthor().toLowerCase().contains(authorSearch))
                 .filter(book -> titleSearch == null || book.getTitle().toLowerCase().contains(titleSearch)).toList();
     }
 
     public Book addNewBook(Book book) {
-        var newId = books.stream().mapToLong(Book::getId).max().orElse(1); // first book id is 1
-        Book newBook = new Book(newId, book.getTitle(), book.getAuthor());
-        books.add(newBook);
-        return newBook;
+        return repository.save(book);
     }
 
     public Book updateBook(Long id, Book newBookData) {
         Book targetBook = getBookById(id);
         targetBook.setTitle(newBookData.getTitle());
         targetBook.setAuthor(newBookData.getAuthor());
-        return targetBook;
+        return repository.save(targetBook);
     }
 
     public void deleteBook(Long id){
         Book targetBook = getBookById(id);
-        books.remove(targetBook);
+        repository.delete(targetBook);
     }
 }
